fd2f0ea56f2cc506b0c2aaa59a8ca9396ca34cdd
Vitor Kawamura Bassani <77632846+vitor-kb@users.noreply.github.com>
2022-08-18 21:35:09 -0300

    Add files via upload

---
+++ ExercicioAlgoritmo/src/exercicioalgoritmo/ExercicioAlgoritmo.java
@@ -0,0 +1,17 @@
+package exercicioalgoritmo;
+import java.util.Scanner;
+public class ExercicioAlgoritmo {
+    public static void main(String[] args) {
+        String nome;
+        double nota1, nota2, media;
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("Digite seu nome: ");
+        nome = entrada.nextLine();
+        System.out.println("Digite a nota 1: ");
+        nota1 = entrada.nextDouble();
+        System.out.println("Digite a nota 2: ");
+        nota2 = entrada.nextDouble();
+        media = (nota1 + nota2) / 2;
+        System.out.println("A media de " + nome + " e: " + media);
+    }
+}
